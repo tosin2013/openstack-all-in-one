deb04b815c40151b33e3f68670ecc8db0b5d732e
Tosin Akinosho <tosin.akinosho@gmail.com>
2022-03-28 13:26:02 -0400

    updating configure code

--- configure-openstack-subs.sh
+++ configure-openstack-subs.sh
@@ -1,24 +1,24 @@
 #!/bin/bash 
 set -xe
 
+sudo dnf install -y dnf-utils
 sudo subscription-manager repos --disable=*
 sudo subscription-manager repos \
---enable=rhel-8-for-x86_64-baseos-rpms \
---enable=rhel-8-for-x86_64-appstream-rpms \
---enable=rhel-8-for-x86_64-highavailability-rpms \
+--enable=rhel-8-for-x86_64-baseos-eus-rpms \
+--enable=rhel-8-for-x86_64-appstream-eus-rpms \
+--enable=rhel-8-for-x86_64-highavailability-eus-rpms \
 --enable=ansible-2.9-for-rhel-8-x86_64-rpms \
 --enable=openstack-16.2-for-rhel-8-x86_64-rpms \
---enable=fast-datapath-for-rhel-8-x86_64-rpms \
---enable=advanced-virt-for-rhel-8-x86_64-rpms || exit $?
+--enable=fast-datapath-for-rhel-8-x86_64-rpms || exit $?
 
 sudo dnf module disable -y container-tools:rhel8
 sudo dnf module enable -y container-tools:3.0
-sudo dnf module disable -y virt:rhel
-sudo dnf module enable -y virt:8.4
+sudo dnf module enable -y virt:rhel
+sudo dnf update -y 
 
 sudo yum install -y python3-tripleoclient
 sudo dnf install -y bind-utils vim git ncurses-devel curl wget tmux net-tools
-sudo dnf update -y 
+
 
 if [ ! -d /usr/share/ceph-ansible ];
 then 
